cb630a7a085ce1fb835cbe4b70d6768e3fd3c8bd
Burnt-Biscuits <78766134+burnt-biscuits@users.noreply.github.com>
2021-03-24 09:21:38 -0500

    Update 0-setup.sh

--- 0-setup.sh
+++ 0-setup.sh
@@ -7,33 +7,6 @@
 #  Arch Linux Post Install Setup and Config
 #-------------------------------------------------------------------------
 
-if ! source install.conf; then
-	read -p "Please enter hostname:" hostname
-
-	read -p "Please enter username:" username
-
-	read -sp "Please enter password:" password
-
-	read -sp "Please repeat password:" password2
-
-	# Check both passwords match
-	if [ "$password" != "$password2" ]; then
-	    echo "Passwords do not match"
-	    exit 1
-	fi
-  printf "hostname="$hostname"\n" >> "install.conf"
-  printf "username="$username"\n" >> "install.conf"
-  printf "password="$password"\n" >> "install.conf"
-fi
-
-echo "-------------------------------------------------"
-echo "Setting up mirrors for optimal download - US Only"
-echo "-------------------------------------------------"
-pacman -Sy
-pacman -S --noconfirm pacman-contrib curl
-pacman -S reflector rsync curl
-# Now generate mirrorlist. Note: If you are not in the U.S. change "United States" to your own country.
-reflector --verbose --country 'United States' -l 5 --sort rate --save /etc/pacman.d/mirrorlist
 nc=$(grep -c ^processor /proc/cpuinfo)
 echo "You have " $nc" cores."
 echo "-------------------------------------------------"
@@ -41,22 +14,3 @@ echo "Changing the makeflags for "$nc" cores."
 sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$nc"/g' /etc/makepkg.conf
 echo "Changing the compression settings for "$nc" cores."
 sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g' /etc/makepkg.conf
-
-echo "-------------------------------------------------"
-echo "       Setup Language to US and set locale       "
-echo "-------------------------------------------------"
-sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-locale-gen
-timedatectl --no-ask-password set-timezone America/Chicago
-timedatectl --no-ask-password set-ntp 1
-localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_COLLATE="" LC_TIME="en_US.UTF-8"
-
-# Set keymaps
-localectl --no-ask-password set-keymap us
-
-# Hostname
-hostnamectl --no-ask-password set-hostname $hostname
-
-# Add sudo no password rights
-sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
-
